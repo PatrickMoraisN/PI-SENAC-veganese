b65e1d0c549bbbcb59e08658bc3dc9a7220f2c3b
Patrick Morais <ppternunes@gmail.com>
2022-11-17 13:40:32 -0300

    feat(styles): add themes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+  white: '#FFF',
+
+  'gray-50': '#FAFAFA',
+  'gray-100': '#E1E1E6',
+  'gray-200': '#DEDEDE',
+  'gray-300': '#C4C4CC',
+  'gray-400': '#8D8D99',
+  'gray-500': '#7C7C8A',
+  'gray-600': '#323238',
+  'gray-700': '#29292E',
+  'gray-800': '#202024',
+  'gray-900': '#121214',
+
+  'green-300': '#00B37E',
+  'green-500': '#00875F',
+  'green-700': '#015F43',
+
+  'red-500': '#AB222E',
+  'red-700': '#7A1921',
+
+  'yellow-100': '#f1e9c9',
+  'yellow-300': '#ffd60a',
+  'yellow-400': '#fcbf49',
+  'yellow-500': '#FBA94C',
+
+  'purple-100': '#ebe5f9',
+}
