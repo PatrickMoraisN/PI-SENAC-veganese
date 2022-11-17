519043d5f9af7268e2ca484cfc1de762d1b191bf
Patrick Morais <ppternunes@gmail.com>
2022-11-17 13:53:21 -0300

    feat(FoodCart reducer): add actions

---
+++ src/reducers/FoodCart/actions.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export enum ActionTypes {
+  ADD_ONE_FOOD = 'ADD_FOOD',
+  REMOVE_ONE_FOOD = 'REMOVE_ONE_FOOD',
+  REMOVE_ALL_TYPE_OF_A_FOOD = 'REMOVE_ALL_TYPE_OF_A_FOOD',
+}
